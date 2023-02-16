52c0ceec072e476f1257a8433c111012ebabfa49
vaici <i.vaicius1@newcastle.ac.uk>
2023-02-16 02:22:52 +0000

    add function for sending user header to the db

--- src/Pages/register/Register.js
+++ src/Pages/register/Register.js
@@ -1,9 +1,25 @@
 // TODO: add css styling
 import RegisterForm from "../../Components/Form/RegisterForm";
 function RegisterPage(){
+    async function addRegisterHandler(registerData){
+        fetch('/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(registerData)
+          })
+            .then(response => response.json())
+            .then(data => {
+              console.log(data);
+              alert('Registration successful!');
+            })
+            .catch(error => {
+              console.error(error);
+              alert('Registration failed.');
+            });
+    }
     return(
         <div>
-            <RegisterForm/>
+            <RegisterForm addRegisterForm={addRegisterHandler}/>
         </div>
     );
 }
